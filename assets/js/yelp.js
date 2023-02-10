7de6cdb5b33620e38d815d24718bb76ac0f3b9a2
Russell Errazo <russellerrazo@gmail.com>
2023-02-10 01:18:00 -0500

    can use space and commas in search bar now

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -1,7 +1,5 @@
 var yelpData;
 var radarData;
-
-
 const yelpPull = {
   method: 'GET',
   headers: {
@@ -10,26 +8,6 @@ const yelpPull = {
     Authorization: 'Bearer JK-RqzXoJPyuvbC0_JQi-xzJH6o2Cx-3tsAEJsOZTWMstIYA93d9ydWjehbT09mEsDG_hnFb3_ooZ0Ha7_Yz-m4ICG3LcfGkcWriLAcKvFJbYyiwvqFP-NUYd0DkY3Yx'
   }
 };
-
-var city = "Orlando"; //placeholder for user input
-var place = "Starbucks"; //placeholder for user input
-
-fetch('https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?location='+city+'&term='+place+'&sort_by=best_match&limit=20', yelpPull)
-  .then(response => response.json())
-  .then(function (response) {
-    console.log(response)
-    yelpData = response
-    var radarLat = yelpData.businesses[0].coordinates.latitude;
-    console.log(radarLat);
-  }
-  )
-  .catch(err => console.error(err));
-/*
-  console.log(yelpData);
-  var radarLat = yelpData.businesses[0].coordinates.latitude;
-  var radarLong;
-  
-
 const radarPull = {
   method: 'GET',
   headers: {
@@ -38,8 +16,27 @@ const radarPull = {
     Authorization: 'prj_test_sk_ed1220433b48d0a1871616fb8d24d7a8a45a34d9'
   }
 };
-
-fetch('https://api.radar.io/v1/search/autocomplete?query='+city, radarPull)
+var city = "orlando"; //placeholder for user input
+var place = "starbucks"; //placeholder for user input
+//the following functions will take the location input and business input and run it through getYelp
+//which will fetch datat from both Yelp and Radar for the relavent search inputs
+function getYelp (town, business) {
+fetch('https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?location='+town+'&term='+business+'&sort_by=best_match&limit=20', yelpPull)
+  .then(response => response.json())
+  .then(function (response) {
+    console.log(response)
+    yelpData = response
+    var radarLong = yelpData.businesses[0].coordinates.longitude.toString();
+    var radarLat = yelpData.businesses[0].coordinates.latitude.toString();
+    var brand = business.toString();
+    getRadar (brand, radarLat, radarLong)
+  }
+  )
+  .catch(err => console.error(err));
+}
+  
+function getRadar (company, xCord, yCord) { //I'm trying to figure out how to get the values from getYelp to work in here
+fetch('https://api.radar.io/v1/search/places?chains='+company+'&near='+xCord+'%2C'+yCord+'&radius=10000', radarPull)
   .then(response => response.json())
   .then(function (response) {
     console.log(response)
@@ -47,4 +44,6 @@ fetch('https://api.radar.io/v1/search/autocomplete?query='+city, radarPull)
   }
   )
   .catch(err => console.error(err));
-  */
+}
+
+getYelp (city, place)
