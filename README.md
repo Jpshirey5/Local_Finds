17695e67f3554957b09e664815d1cf0a269ff0c2
Austin Nobregas <austinnobregas@gmail.com>
2023-02-13 18:52:21 -0500

    read.me

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Project_1
+#local finds
+
+This application is desighned to help users broaden their nightlife routine and experience something new. It uses multiple web APIs to bring multiple applications into one. 
+
+
+## Authors
+
+- [@anobregas](https://github.com/Anobregas/)
+- [@ginitadavis](https://github.com/ginitadavis/)
+- [@Russell-Errazo](https://github.com/Russell-Errazo/)
+- [@Jpshirey5](https://github.com/Jpshirey5)
+
+
+
+## Deployment
+
+To deploy this project run https://jpshirey5.github.io/Project_1/
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+## Demo
+
+https://jpshirey5.github.io/Project_1/
+
+
+## 🚀 About Us
+we are a team of full stack developers who are currently building a solid portfolio in order to advance our carrers as sofware engineers. 
+
+
+## API Reference
+
+
+ - [geofence](https://api.radar.io/v1/search/geofences?near=28.606464,-81.5235072)
+ - [materializeCSS](https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css)
+
+
+## Screenshots
+
+![App Screenshot](/assets/images/Screenshot%202023-02-13%20at%206.44.51%20PM.png)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,118 @@
-var apiKey = 'Authorization: prj_test_sk_ed1220433b48d0a1871616fb8d24d7a8a45a34d9';
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+var globalData = {};
+
+//Sets the map to Orlando, Florida as default
+var map = L.map('map').setView([28.5384, -81.3789], 13);
+
+//Radar's Api Key
+var apiKey = 'prj_test_sk_ed1220433b48d0a1871616fb8d24d7a8a45a34d9';
+
+//Radar's url
 var url = 'https://api.radar.io/v1/search/autocomplete?query=';
+var url2 = 'https://api.radar.io/v1/search/autocomplete?query=';
 var searchBtn = document.querySelector('.searchBtn');
 
-searchBtn.addEventListener('click', handleSearchFormSubmit);
+searchBtn.addEventListener('click', handleSearch);
 
-function handleSearchFormSubmit(event) {
+function handleSearch(event) {
     event.preventDefault();
 
-    var valueEntered = document.querySelector('.userInput').value;
+    //User input: city or place
+    var cityEntered = document.querySelectorAll('.userInput')[0].value;
+    var placeNearByEntered = document.querySelectorAll('.userInput')[1].value;
 
-    if (!userInput) {
-        // Nothing happens if the user doesn't enter information
+    if (!cityEntered && !placeNearByEntered) {
+        // If the input text is invalid, show the modal with an error message
+        modalMessage.innerHTML = "Please enter a city or a place to search.";
+        modal.style.display = "block";
         return null;
-    } 
+      }
 
-    getApi(valueEntered);
+    getApi(cityEntered, placeNearByEntered);
 
 }
 
-function getApi(userInput){
+//Once user clicks screen the window with error message hides
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-    //Get the url
-    var queryString = url + userInput + apiKey;
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
 
-    fetch(queryString)
-        .then(function (response) {
-            if (!response.ok) {
-                // window.alert('Not a valid city');
-                throw new Error('Network response was not ok'); //response.json();
-            }
-            return response.json();
-        })
+function getApi(userInput, userInput2){
+
+    //Radar.com gets the place or city's coordinates that will be displayed in the map
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          "Access-Control-Allow-Origin" : "*",
+          Authorization: apiKey
+        }
+      };
+
+      var queryUrl;
+      //Create queryUrl
+      if (userInput){ //If city has value, but the place nearby is empty
+        queryUrl = url + userInput;
+      }
+      
+      fetch(queryUrl, options)
+        .then(response => response.json())
         .then(function (data) {
-            //if fetch was successful
-            console.log("Fetch work");
+            //my code goes here
+            console.log(data);
+            var lat = data.addresses[0].latitude;
+            var lng = data.addresses[0].longitude;
+            
+            setUserEnteredLocation(lat, lng);
+
+            var geofence = 'https://api.radar.io/v1/search/geofences?near=28.606464,-81.5235072'// + lat + ',' + lng;
+            console.log('geofence ' + geofence);
+            console.log(options);
+
+            var placeNearBy = document.getElementById('placeNearBy');
+            placeNearBy.readOnly = false;
+            placeNearBy.setAttribute('style', 'background-color: white');
+
+            //Below are the user tracked location
+            // console.log(globalData.latitude);
+            // console.log(globalData.longitude);
 
         })
-        .catch(error => {
-            console.error('There was a problem:', error);
-        });
+        .catch(err => console.error(err));
 
+
+}
+
+navigator.geolocation.getCurrentPosition(successDetectedLocation, 
+    errorLocation, {
+    enableHighAccuracy: true
+})
+
+function successDetectedLocation(position){
+    console.log(position)
+    map.setView([position.coords.latitude, position.coords.longitude], 13)
+    placeNearBy.readOnly = false;
+    placeNearBy.setAttribute('style', 'background-color: white');
+    globalData.latitude = position.coords.latitude;
+    globalData.longitude = position.coords.longitude;
+}
+
+function errorLocation(){
+    console.log('User does not allow to track their location')
+}
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function setUserEnteredLocation(posLatitude, posLongitude){
+    map.setView([posLatitude, posLongitude], 13)
 }
